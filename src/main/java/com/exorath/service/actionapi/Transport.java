274b864181327cac25317449ff9d51db75ca0d54
Toon Sevrin <twan123@live.be>
2016-11-12 18:58:25 +0100

    Service layout

---
+++ src/main/java/com/exorath/service/actionapi/Transport.java
@@ -0,0 +1,12 @@
+package com.exorath.service.actionapi;
+
+import com.exorath.service.commons.portProvider.PortProvider;
+
+/**
+ * Created by toonsev on 11/12/2016.
+ */
+public class Transport {
+    public static void setup(Service service, PortProvider portProvider){
+
+    }
+}
---
+++ src/main/java/com/exorath/service/actionapi/impl/SimpleService.java
@@ -0,0 +1,10 @@
+package com.exorath.service.actionapi.impl;
+
+import com.exorath.service.actionapi.Service;
+
+/**
+ * Created by toonsev on 11/12/2016.
+ */
+public class SimpleService implements Service{
+
+}
